db73ea072bd569052ac70c842b011e4b2bb327a1
LachlanNewman <lachnewman007@gmail.com>
2022-06-01 14:40:56 +0800

    test mmd file

---
+++ scripts/generateSVGs.sh
@@ -0,0 +1,6 @@
+for i  in `find ./diagrams -type f -name "*.mmd"`; do
+    file=$(basename "$i")
+    filename=${file%.*}
+    echo "$filename"
+    yarn mmdc -i ./diagrams/${filename}.mmd -o ./diagrams/${filename}.svg
+done
