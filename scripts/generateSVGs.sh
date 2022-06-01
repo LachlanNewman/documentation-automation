2b3d0bd41c521990215982a64588cfa6345a7cb2
LachlanNewman <LachlanNewman@users.noreply.github.com>
2022-06-01 07:06:16 +0000

    Automated Change

--- scripts/generateSVGs.sh
+++ scripts/generateSVGs.sh
@@ -2,5 +2,5 @@ for i  in `find ./diagrams -type f -name "*.mmd"`; do
     file=$(basename "$i")
     filename=${file%.*}
     echo "$filename"
-    yarn mmdc -i ./diagrams/${filename}.mmd -o ./diagrams/${filename}.svg
+    yarn mmdc -i ./diagrams/${filename}.mmd -o ./diagrams/${filename}.svg -f
 done
